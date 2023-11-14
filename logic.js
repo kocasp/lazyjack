46e7edbe1a7b583ab1637cb151b207d9743d8be0
Konrad <kocasp@gmail.com>
2023-11-14 03:12:24 +0100

    Modernize

---
+++ logic.js
@@ -0,0 +1,77 @@
+const slider = document.getElementById('slider');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+const commentTextarea = document.getElementById('commentTextarea');
+const keys = Object.keys(jsonData);
+let currentIndex = 0;
+const buttonsContainer = document.createElement('div');
+
+function appendToTextarea(text, newline) {
+    commentTextarea.value += (newline ? '\n' : '') + text;
+}
+
+function createButton(text, newline) {
+    const button = document.createElement('button');
+    button.textContent = text;
+    button.addEventListener('click', () => appendToTextarea(text, newline));
+    return button;
+}
+
+function createDropdown(part) {
+    const dropdownContainer = document.createElement('div');
+    dropdownContainer.classList.add('dropdown-container');
+
+    const dropdownSelect = document.createElement('select');
+    dropdownSelect.classList.add('dropdown-select');
+    part.choices.forEach(choice => {
+        const option = document.createElement('option');
+        option.value = choice;
+        option.textContent = choice;
+        dropdownSelect.appendChild(option);
+    });
+    dropdownContainer.appendChild(dropdownSelect);
+
+    const okButton = document.createElement('button');
+    okButton.textContent = 'OK';
+    okButton.classList.add('dropdown-button');
+    okButton.addEventListener('click', () => {
+        appendToTextarea(dropdownSelect.value, part.newline);
+    });
+    dropdownContainer.appendChild(okButton);
+
+    return dropdownContainer;
+}
+
+function updateSlide() {
+    slider.textContent = keys[currentIndex];
+    buttonsContainer.innerHTML = ''; // Remove previous buttons
+
+    const slide = jsonData[keys[currentIndex]];
+    if (slide) {
+        slide.forEach(item => {
+            item.parts.forEach(part => {
+                if (part.text) {
+                    buttonsContainer.appendChild(createButton(part.text, part.newline));
+                } else if (part.choices) {
+                    buttonsContainer.appendChild(createDropdown(part));
+                }
+            });
+            buttonsContainer.appendChild(document.createElement("br"));
+            buttonsContainer.appendChild(document.createElement("br"));
+        });
+    }
+
+    commentTextarea.insertAdjacentElement('afterend', buttonsContainer);
+    prevBtn.disabled = currentIndex === 0;
+    nextBtn.disabled = currentIndex === keys.length - 1;
+}
+
+function changeSlide(step) {
+    currentIndex = (currentIndex + step + keys.length) % keys.length;
+    updateSlide();
+}
+
+prevBtn.addEventListener('click', () => changeSlide(-1));
+nextBtn.addEventListener('click', () => changeSlide(1));
+
+updateSlide();
---
+++ scenarios.js
@@ -0,0 +1,424 @@
+const jsonData = {
+  "Rodzaj badania": [
+    {
+      "name": "0.1",
+      "parts": [
+        {
+          "text": "TK zatok obocznych nosa bez dożylnego podania środka kontrastowego.",
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "0.2",
+      "parts": [
+        {
+          "text": "TK zatok obocznych nosa z dożylnym podaniem środka kontrastowego.",
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "0.3",
+      "parts": [
+        {
+          "text": "Dane ze skierowania:",
+          "newline": true
+        }
+      ]
+    }
+  ],
+  "Zatoki czołowe": [
+    {
+      "name": "1.1",
+      "parts": [
+        {
+          "choices": [
+            "Zatoki oboczne nosa wykształcone prawidłowo.",
+            "Ubytki ścian przyśrodkowych zatok szczękowych, ściany przedniej zatoki klinowej, przegrody nosa, komórek sitowia oraz asymetria małżowin nosowych środkowych- o obrazie zmian pooperacyjnych."
+          ],
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "1.2",
+      "parts": [
+        {
+          "choices": [
+            "Zatoki czołowe o zachowanej powietrzności.",
+            "Hipoplazja prawej zatoki czołowej.",
+            "Hipoplazja lewej zatoki czołowej.",
+            "Hipoplazja zatok czołowych."
+          ],
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "1.3",
+      "parts": [
+        {
+          "choices": [
+            "Drobne pogrubienia błon śluzowych zatok czołowych od strony zachyłków zębodołowych.",
+            "Drobne pogrubienia błony śłuzowej prawej zatoki czołowej od strony zachyłka zębodołowego.",
+            "Drobne pogrubienia błony śłuzowej lewej zatoki czołowej od strony zachyłka zębodołowego."
+          ],
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "1.4",
+      "parts": [
+        {
+          "choices": [
+            "Zachyłki czołowe o zachowanej drożności.",
+            "Zachyłki czołowe: po stronie prawej o zachowanej drożności, po stronie lewej niedrożny na tle pogrubień błon śluzowych.",
+            "Zachyłki czołowe: po stronie prawej niedrożny na tle pogrubień błon śluzowych, po stronie lewej o zachowanej drożności."
+          ],
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "1.5",
+      "parts": [
+        {
+          "text": "Wpuklanie komórek powietrznych sitowia przedniego w kierunku zatok czołowych.",
+          "newline": true
+        }
+      ]
+    }
+  ],
+  "Sitowie": [
+    {
+      "name": "2.1",
+      "parts": [
+        {
+          "text": "Sitowie o zachowanej powietrzności.",
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "2.2",
+      "parts": [
+        {
+          "text": "Drobne pogrubienia błon śluzowych sitowia z bezpowietrzniścią pojedynczych komórek sitowia.",
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "2.3",
+      "parts": [
+        {
+          "choices": [
+            "Głębokość rowków węchowych wg. klasyfikacji Kerosa: po stronie prawej 1mm-typ I,",
+            "Głębokość rowków węchowych wg. klasyfikacji Kerosa: po stronie prawej 2mm-typ I,",
+            "Głębokość rowków węchowych wg. klasyfikacji Kerosa: po stronie prawej 3mm-typ I,",
+            "Głębokość rowków węchowych wg. klasyfikacji Kerosa: po stronie prawej 4mm-typ II,",
+            "Głębokość rowków węchowych wg. klasyfikacji Kerosa: po stronie prawej 5mm-typ II,",
+            "Głębokość rowków węchowych wg. klasyfikacji Kerosa: po stronie prawej 6mm-typ II,",
+            "Głębokość rowków węchowych wg. klasyfikacji Kerosa: po stronie prawej 7mm-typ II,",
+            "Głębokość rowków węchowych wg. klasyfikacji Kerosa: po stronie prawej 8mm-typ III,",
+            "Głębokość rowków węchowych wg. klasyfikacji Kerosa: po stronie prawej 9mm-typ III,",
+            "Głębokość rowków węchowych wg. klasyfikacji Kerosa: po stronie prawej 10mm-typ III,",
+            "Głębokość rowków węchowych wg. klasyfikacji Kerosa: po stronie prawej 11mm-typ III,",
+            "Głębokość rowków węchowych wg. klasyfikacji Kerosa: po stronie prawej 12mm-typ III,",
+            "Głębokość rowków węchowych wg. klasyfikacji Kerosa: po stronie prawej 13mm-typ III,",
+            "Głębokość rowków węchowych wg. klasyfikacji Kerosa: po stronie prawej 14mm-typ III,",
+            "Głębokość rowków węchowych wg. klasyfikacji Kerosa: po stronie prawej 15mm-typ III,",
+            "Głębokość rowków węchowych wg. klasyfikacji Kerosa: po stronie prawej 16mm-typ III,"
+          ],
+          "newline": false
+        },
+        {
+          "choices": [
+            " po stronie lewej 1mm-typ I.",
+            " po stronie lewej 2mm-typ I.",
+            " po stronie lewej 3mm-typ I.",
+            " po stronie lewej 4mm-typ II.",
+            " po stronie lewej 5mm-typ II.",
+            " po stronie lewej 6mm-typ II.",
+            " po stronie lewej 7mm-typ II.",
+            " po stronie lewej 8mm-typ III.",
+            " po stronie lewej 9mm-typ III.",
+            " po stronie lewej 10mm-typ III.",
+            " po stronie lewej 11mm-typ III.",
+            " po stronie lewej 12mm-typ III.",
+            " po stronie lewej 13mm-typ III.",
+            " po stronie lewej 14mm-typ III.",
+            " po stronie lewej 15mm-typ III.",
+            " po stronie lewej 16mm-typ III."
+          ],
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "2.4",
+      "parts": [
+        {
+          "choices": [
+            "Przebieg kanału tętnicy sitowej przedniej prawej poniżej stropu sitowia.",
+            "Przebieg kanału tętnicy sitowej przedniej lewej poniżej stropu sitowia.",
+            "Przebieg kanałów tętnicy sitowych przednich poniżej stropu sitowia."
+          ],
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "2.5",
+      "parts": [
+        {
+          "text": "Wpuklanie komórek sitowia nadoczodołowo obustronnie.",
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "2.6",
+      "parts": [
+        {
+          "choices": [
+            "Prawowypukłe skrzywienie przegrody nosa z towarzyszącą ostrogą kostną od strony wypukłej.",
+            "Lewowypukłe skrzywienie przegrody nosa z towarzyszącą ostrogą kostną od strony wypukłej.",
+            "Esowate skrzywienie przegrody nosa."
+          ],
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "2.7",
+      "parts": [
+        {
+          "choices": [
+            "Komórka Onodiego po stronie prawej.",
+            "Komórka Onodiego po stronie lewej.",
+            "Komórki Onodiego obustronnie."
+          ],
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "2.8",
+      "parts": [
+        {
+          "text": "Drobne pogrubienia błon śłuzowych przewodów nosowych.",
+          "newline": true
+        }
+      ]
+    }
+  ],
+  "Zatoka klinowa": [
+    {
+      "name": "3.1",
+      "parts": [
+        {
+          "text": "Zatoka klinowa prawidłowo powietrzna.",
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "3.2",
+      "parts": [
+        {
+          "choices": [
+            "Drobne pogrubienia błon śluzowych zatoki klinowej obustronnie.",
+            "Drobne pogrubienia błon śluzowych zatoki klinowej po stronie prawej.",
+            "Drobne pogrubienia błon śluzowych zatoki klinowej po stronie lewej."
+          ],
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "3.3",
+      "parts": [
+        {
+          "choices": [
+            "Drobne pogrubienia błon śluzowych zatoki klinowej w okolicy jej ujść.",
+            "Drobne pogrubienia błony śluzowej zatoki klinowej w okolicy jej ujścia po stronie prawej.",
+            "Drobne pogrubienia błony śluzowej zatoki klinowej w okolicy jej ujścia po stronie lewej."
+          ],
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "3.4",
+      "parts": [
+        {
+          "choices": [
+            "Zachyłki klinowo-sitowe obustronnie o zachowanej drożności.",
+            "Zachyłki klinowo-sitowe: prawy o zachowanej drożności, lewy niedrożny na tle pogrubień błon śluzowych.",
+            "Zachyłki klinowo-sitowe: prawy niedrożny na tle pogrubień błon śluzowych, lewy o zachowanej drożności."
+          ],
+          "newline": true
+        }
+      ]
+    }
+  ],
+  "Zatoki szczękowe": [
+    {
+      "name": "4.1",
+      "parts": [
+        {
+          "text": "Zatoki szczękowe o zachowanej powietrzności.",
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "4.2",
+      "parts": [
+        {
+          "choices": [
+            "Pogrubienie błony śluzowej zatoki szczękowej prawej od strony zachyłka zębodołowego grubości do mm.",
+            "Pogrubienie błony śluzowej zatoki szczękowej lewej od strony zachyłka zębodołowego grubości do mm.",
+            "Pogrubienia błon śluzowych zatok szczękowych od strony zachyłków zębodołowych grubości do mm.",
+            "Drobne pogrubienia błon śluzowych zatok szczękowych od strony zachyłków zębodołowych."
+          ],
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "4.3",
+      "parts": [
+        {
+          "choices": [
+            "Kompleksy ujściowo-przewodowe o zachowanej drożności.",
+            "Kompleksy ujściowo-przewodowe: po stronie prawej o zachowanej drożności, po stronie lewej niedrożny na tle pogrubień błon śluzowych.",
+            "Kompleksy ujściowo-przewodowe: po stronie prawej niedrożny na tle pogrubień błon śluzowych, po stronie lewej o zachowanej drożności.",
+            "Kompleksy ujściowo-przewodowe obustronnie niedrożne na tle pogrubień błon śluzowych."
+          ],
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "4.4",
+      "parts": [
+        {
+          "choices": [
+            "Dodatkowe ujście zatoki szczękowej prawej na poziomie małżowiny nosowej środkowej.",
+            "Dodatkowe ujście zatoki szczękowej lewej na poziomie małżowiny nosowej środkowej.",
+            "Dodatkowe ujścia zatok szczękowych na poziomie małżowin nosowych środkowych."
+          ],
+          "newline": true
+        }
+      ]
+    }
+  ],
+  "Wyrostki sutkowate": [
+    {
+      "name": "5.1",
+      "parts": [
+        {
+          "text": "Wyrostki sutkowate kości skroniowych o zachowanej powietrzności.",
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "5.2",
+      "parts": [
+        {
+          "choices": [
+            "Częściowa bezpowietrzność komórek wyrostka sutkowatego prawej kości skroniowej.",
+            "Częściowa bezpowietrzność komórek wyrostka sutkowatego lewej kości skroniowej.",
+            "Częściowa bezpowietrzność komórek wyrostków sutkowatych obu kości skroniowych."
+          ],
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "5.3",
+      "parts": [
+        {
+          "choices": [
+            "Zwiększona sklerotyzacja wyrostka sutkowatego prawej kości skroniowej.",
+            "Zwiększona sklerotyzacja wyrostka sutkowatego lewej kości skroniowej.",
+            "Zwiększona sklerotyzacja wyrostków sutkowatych kości skroniowych."
+          ],
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "5.4",
+      "parts": [
+        {
+          "text": "Poza tym wyrostki sutkowate o zachowanej powietrzności.",
+          "newline": true
+        }
+      ]
+    },
+    {
+      "name": "5.5",
+      "parts": [
+        {
+          "text": "Elementy kostne ścian zatok są prawidłowe.",
+          "newline": true
+        }
+      ]
+    }
+  ],
+  "Tkanki miękkie/mózgowie- czy widzisz zmiany?": [
+    {
+      "name": "6.1",
+      "parts": [
+        {
+          "text": " ",
+          "newline": true
+        }
+      ]
+    }
+  ],
+  "Wnioski": [
+    {
+      "name": "7.1",
+      "parts": [
+        {
+          "text": "Wnioski: ",
+          "newline": false
+        }
+      ]
+    },
+    {
+      "name": "7.2",
+      "parts": [
+        {
+          "text": "Pogrubienia błon śluzowych zatok obocznych nosa jak w opisie badania.",
+          "newline": false
+        }
+      ]
+    },
+    {
+      "name": "7.3",
+      "parts": [
+        {
+          "text": "Pozostałe wnioski oraz szczegóły zawarte w opisie badania.",
+          "newline": false
+        }
+      ]
+    },
+    {
+      "name": "7.4",
+      "parts": [
+        {
+          "text": "Zawarte w opisie badania.",
+          "newline": false
+        }
+      ]
+    }
+  ]
+};
